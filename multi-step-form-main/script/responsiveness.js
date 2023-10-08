d946285cfe382dd314a856f5a8a3803381aaf783
flcristian <qflorescucristian@gmail.com>
2023-10-08 18:00:27 +0300

    Finished multi step form challenge

--- multi-step-form-main/script/responsiveness.js
+++ multi-step-form-main/script/responsiveness.js
@@ -3,12 +3,19 @@ function setFooterContainer() {
     const mainContainer = document.getElementById("main-container");
     const formContainer = document.getElementById("form-container");
     const image = document.getElementById("background-img");
+    const menuLabels = document.getElementsByClassName("menu-option-label-container");
     if (window.innerWidth > 1000) {
         formContainer.appendChild(footer);
         image.src = "assets/images/bg-sidebar-desktop.svg";
+        for(var i = 0;i<menuLabels.length;i++){
+            menuLabels[i].style.display = "flex";
+        }
     } else {
         mainContainer.appendChild(footer);
         image.src = "assets/images/bg-sidebar-mobile.svg";
+        for(var i = 0;i<menuLabels.length;i++){
+            menuLabels[i].style.display = "none";
+        }
     }
 }
 
